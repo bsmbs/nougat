96e7267fdc0e70c7ad8189af4dfca8816d472bff
pizza61 <34103636+pizza61@users.noreply.github.com>
2018-05-03 22:10:54 +0200

    fix komendy 8pilka

--- commands/8pilka.js
+++ commands/8pilka.js
@@ -5,7 +5,7 @@ exports.run = (args, message, Discord) => {
     let odpowiedzi = ['Raczej nie!', 'Oczywiście!', 'Ty no nie wiem', 'eee papryka', 'Xiaomi lepsze', 'Jak najbardziej', 'Hehe nie'];
     if(text.toLowerCase().includes("xiaomi")) is = 4; //jesli pytanie zawiera xiaomi, zawsze odpowiadaj xiaomi lepsze
     const mbed = new Discord.RichEmbed()
-        .setAuthor(str.name)
+        .setAuthor("Nougat")
         .setTitle(text)
         .setColor(0x198c41)
         .setDescription(odpowiedzi[is]);
