b9ff8961b1425696e1703146a9d9131519956178
pizza61 <34103636+pizza61@users.noreply.github.com>
2018-05-04 15:01:01 +0200

    \n aby NIE SPAMIŁO NOUGATEM

--- commands/sprzedaj.js
+++ commands/sprzedaj.js
@@ -1,7 +1,6 @@
 exports.run = (message, Discord, sprzedajmode, sprzedajmodev) => {
     message.channel.send("Napisałem do ciebie na pw - tam poprowadzę cię przez proces wystawiania przedmiotu.")
-    message.author.send("Sprzedawanie polega na tym, że wystawiasz coś do *biedronka, a jak ktoś to kupi to dostajesz z tego od 0 do 100%!")
-    message.author.send("Podaj nazwę przedmiotu, który chcesz sprzedać. Nie może być w nim linków!")
+    message.author.send("Sprzedawanie polega na tym, że wystawiasz coś do *biedronka, a jak ktoś to kupi to dostajesz z tego od 0 do 100%!\nPodaj nazwę przedmiotu, który chcesz sprzedać. Nie może być w nim linków!")
         .catch(() => {
             const errEmbed = new Discord.RichEmbed()
                 .setAuthor('Nougat - Biedronka', 'https://pbs.twimg.com/profile_images/3578001181/990ad36a51b8e483cde968adbb53df5a_400x400.png')
