2778224cacc690dea8b4627dbebea0085e6b4eb9
pizza61 <34103636+pizza61@users.noreply.github.com>
2018-05-31 16:21:45 +0200

    update userinfo

--- src/commands/info/help.ts
+++ src/commands/info/help.ts
@@ -6,22 +6,53 @@ export default function help(message: Message) {
     const pomoca = new RichEmbed()
     .setAuthor("Nougat - Prefix: " + config.prefix, 'https://cdn.discordapp.com/avatars/429587398511427584/a8d77ae510e68cc595c1ccda04a755fa.jpg?size=1024')
         .setColor(color.toString(16))
-        .setTitle("https://pizza61.github.io/nougat")
+        .setTitle("Rozszerzona lista komend na: https://pizza61.github.io/nougat")
         .setDescription("Użycie: nazwa komendy <obowiązkowy argument|albo inny> [nieobowiazkowy argument]. \nPamiętaj, aby przed nazwą komendy umieścić prefix. \nUżycie komendy bez potrzebnych arg. może skutkować pojawieniem się wyjasnienia.")
+        .setFooter('Nougat v3.0 (30.05.2018) by Pizza, hostuje '+config.host)
 
-    const pomoc = new RichEmbed()
-    .setAuthor("Nougat - Prefix: " + config.prefix, 'https://cdn.discordapp.com/avatars/429587398511427584/a8d77ae510e68cc595c1ccda04a755fa.jpg?size=1024')
+    const interaktywne = new RichEmbed()
+        .setAuthor("Interaktywne i ekonomia", 'https://cdn.discordapp.com/avatars/429587398511427584/a8d77ae510e68cc595c1ccda04a755fa.jpg?size=1024')
+        .setColor(0x9C27B0)
+        .setDescription('**Interaktywne**: `pozwij`, `sprzedaj`\n`biedronka`, `hajs`, `kup <id>`, `zaplac <wzmianka> <liczba>`');
+    
+    const zabawa = new RichEmbed()
+        .setAuthor("Info", 'https://cdn.discordapp.com/avatars/429587398511427584/a8d77ae510e68cc595c1ccda04a755fa.jpg?size=1024')
+        .setColor(0xf47100)
+        .setDescription('`8pilka <zapytanie>`, `ciastko`, `mono`, `odwroc <tekst>`, `sms <wzmianka> <wiadomosc>`, `statek <wzmianka> <wzmianka>`, `wybierz <oddzielone |>`')
+
+    const info = new RichEmbed()
+        .setAuthor("Zabawa", 'https://cdn.discordapp.com/avatars/429587398511427584/a8d77ae510e68cc595c1ccda04a755fa.jpg?size=1024')
+        .setColor(0x2f7d32)
+        .setDescription('`check`, `checkme`, `git`, `help`, `staty`, `userinfo <wzmianka>`, `yt <kanal>`')
+  
+    const pic = new RichEmbed()
+        .setAuthor("Obrazki", 'https://cdn.discordapp.com/avatars/429587398511427584/a8d77ae510e68cc595c1ccda04a755fa.jpg?size=1024')
+        .setColor(0x1E88E5)
+        .setDescription('`kolory`, `rozjasnij`, `sepia`, `przekrec`')
+        .setFooter("Do każdej komendy dołącz obrazek lub link do obrazka")
+
+    const admin = new RichEmbed()
+        .setAuthor("Administracja", 'https://cdn.discordapp.com/avatars/429587398511427584/a8d77ae510e68cc595c1ccda04a755fa.jpg?size=1024')
+        .setColor(0xD32F2F)
+        .setDescription("`autorole <nazwa roli|usun>`, `czysc <liczba wiadomosci>`, `nazwa <nowa nazwa bota>`, `warn <wzmianka> [powod]`, `zakazane`");
+    
+    /* const pomoc = new RichEmbed()
+        .setAuthor("Nougat - Prefix: " + config.prefix, 'https://cdn.discordapp.com/avatars/429587398511427584/a8d77ae510e68cc595c1ccda04a755fa.jpg?size=1024')
         .setColor(color.toString(16))
-        .addField('I n t e r a k t y w n e', '`pozwij`, `sprzedaj`')
+        //.addField('I n t e r a k t y w n e', '`pozwij`, `sprzedaj`')
         .addField('Ekonomia', '`zaplac <osoba> <kwota>`, `kup <id>`, `biedronka`, `hajs`')
         .addField('Zabawa', '`8pilka <zapytanie>`, `odwroc <tekst do odwrocenia>`, `ciastko`, `statek <pierwszy> <drugi>`, `mono`, `wybierz <co wybrac oddzielone |>`')
         .addField('Społecznościowe', '`sms <osoba> <wiadomosc>`')
         .addField('Informacja', '`yt <nazwa kanalu>`, `userinfo [uzytkownik]`, `git`, `check`, `checkme`')
         .addField('Obrazki', 'Do każdej wiadomości z komendą dołącz obrazek: `kolory`, `rozjasnij`, `sepia`, `przekrec`')
         .addField('Administracja', '`nazwa <nowa nazwa bota>`, `warn <uzytkownik> [powod]`, `czysc <ilosc wiadomosci>`, `zakazane`, `autorole <nazwa roli|usun>`')
-        .setFooter('Nougat v3.0 (30.05.2018) by Pizza, hostuje '+config.host)
-    message.author.send({embed: pomoca})
-    message.author.send({embed: pomoc})
+        .setFooter('Nougat v3.0 (30.05.2018) by Pizza, hostuje '+config.host) */
+        message.author.send({embed: interaktywne})
+        message.author.send({embed: zabawa})
+        message.author.send({embed: info})
+        message.author.send({embed: pic})
+        message.author.send({embed: admin})
+        message.author.send({embed: pomoca})
 }
 
 /*exports.run = (message, Discord, prefix, hostuje) => {
--- src/commands/info/userinfo.ts
+++ src/commands/info/userinfo.ts
@@ -1,13 +1,17 @@
-import { RichEmbed } from 'discord.js';
+import { RichEmbed, User, GuildMember, Message } from 'discord.js';
 
-export default function userinfo(args, message) {
+export default function userinfo(args, message: Message) {
+    if(!message.guild) return;
     let usernazwa = args.slice(0).join(" ");
     if(usernazwa == '') {
-        var uzytkownik = message.author;
+        var uzytkownik: User = message.author;
+        var uzytkownikMem: GuildMember = message.member;
     } else {
-        var uzytkownik = message.mentions.members.first().user
+        var uzytkownik: User = message.mentions.members.first().user;
+        var uzytkownikMem: GuildMember = message.mentions.members.first();
     }
     let czas = new Date(uzytkownik.createdTimestamp);
+    let czasDol = new Date(uzytkownikMem.joinedTimestamp);
     let status = 'Nieznany';
     switch(uzytkownik.presence.status) {
         case 'online':
@@ -30,7 +34,11 @@ export default function userinfo(args, message) {
         .setTitle(uzytkownik.username)
         .addField('Zarejestrowano', `${czas.getDate()}.${czas.getMonth()+1}.${czas.getFullYear()} ${czas.getHours()}:${czas.getMinutes()}`, true)
         .addField('Tag', uzytkownik.discriminator, true)
-        .addField('Status', status, true);
+        .addField('Status', status, true)
+        .addField('Data dołączenia na serwer', `${czasDol.getDate()}.${czasDol.getMonth()+1}.${czasDol.getFullYear()} ${czasDol.getHours()}:${czasDol.getMinutes()}`,true)
+        .addField('ID', uzytkownik.id, true)
+        .addField('Role', uzytkownikMem.roles.array(), true)
+        .addField('Link do avataru:', uzytkownik.avatarURL, false)
     message.channel.send({
         embed: boxdel
     });
