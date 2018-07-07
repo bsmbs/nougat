74ff37fc13d7a0a07bf7945aa24abeb9c7855444
pizza61 <34103636+pizza61@users.noreply.github.com>
2018-07-07 19:39:54 +0200

    fix crasha

--- src/main/main.ts
+++ src/main/main.ts
@@ -105,7 +105,7 @@ export class Nougat {
         this.client.on('guildMemberAdd', member => onJoin(member))
         this.client.on("message", message => {
             if(message.author.bot) {
-                agents(message);
+                //agents(message);
                 return;
             }
             this.dodajPunkty(message);
